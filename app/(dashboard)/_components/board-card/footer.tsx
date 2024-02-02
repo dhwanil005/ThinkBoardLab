71d103465e764792c0598deb5998d45e887bb26b
dhwanil005 <dhwanil.shah00@gmail.com>
2024-02-02 07:04:25 -0500

    Favorite and unfavorite functionality implemented, search functionality implemented, favorite board page now shows only the ones favorited and not all the boards, next is fixing some bugs and work on how an individual board would look like

--- app/(dashboard)/_components/board-card/footer.tsx
+++ app/(dashboard)/_components/board-card/footer.tsx
@@ -1,5 +1,6 @@
 import { Star } from "lucide-react";
 import { cn } from "@/lib/utils";
+import React from "react";
 
 interface FooterProps {
     isFavorite: boolean;
@@ -18,6 +19,15 @@ export const Footer = ({
     onClick,
     disabled
 } : FooterProps) => {
+
+    const handleClick = (
+        event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => {
+        event.stopPropagation();
+        event.preventDefault();
+
+        onClick();
+    }
     return (
         <div className="relative bg-white p-3">
             <p className="text-[13px] truncate max-w-[calc(100%-20px)]">
@@ -26,7 +36,7 @@ export const Footer = ({
             <p className="opacity-0 group hover:opacity-100 transition-opacity text-[11px] text-muted-foreground truncate">
                 {authorLabel} • {createdAtLabel}
             </p>
-            <button disabled={disabled} onClick={onClick}
+            <button disabled={disabled} onClick={handleClick}
                 className={cn(
                     "opacity-0 group-hover:opacity-100 transition absolute top-3 right-3 text-muted-foreground hover:text-blue-600",
                     disabled && "cursor-not-allowed opacity-75"
--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -7,8 +7,10 @@ import { Organization, organizations } from "@clerk/nextjs/api";
 import { useOrganization } from "@clerk/nextjs";
 import { useApiMutation } from "@/hooks/use-api-mutation";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 export const EmptyBoards = () => {
+    const router = useRouter();
     const {organization} = useOrganization();
     const {mutate, pending} = useApiMutation(api.board.create);
 
@@ -21,6 +23,7 @@ export const EmptyBoards = () => {
         })
         .then((id) => {
             toast.success("Board created")
+            router.push(`/board/${id}`)
         })
         .catch(()=>{
             toast.error("Failed to create board")
