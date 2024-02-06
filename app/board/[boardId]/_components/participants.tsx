e2b4415e9150c92217a6bf82b0ee5737ce0d1140
dhwanil005 <dhwanil.shah00@gmail.com>
2024-02-05 20:05:27 -0500

    Users displayed using userAvatar, borderColor randomly decided using connectionId, total number of users using max-shown-users

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,8 +1,49 @@
+'use client';
+
+import { useOther, useOthers, useSelf } from "@/liveblocks.config";
+import { UserAvatar } from "./user-avatar";
+import { currentUser } from "@clerk/nextjs";
+import { connectionIdToColor } from "@/lib/utils";
+
+const MAX_SHOWN_OTHER_USERS = 2;
 
 export const Participants = () => {
+    const users = useOthers();
+    const currentUser = useSelf();
+    const hasMoreUsers = users.length > MAX_SHOWN_OTHER_USERS;
+
     return(
         <div className="absolute h-12 top-2 right-2 bg-white p-3 flex items-center rounded-md shadow-md">
-            list of users
+            <div className="flex gap-x-2">
+                {users.slice(0,MAX_SHOWN_OTHER_USERS)
+                    .map(({connectionId, info})=>{
+                        return (
+                            <UserAvatar
+                            key = {connectionId}
+                            src = {info?.picture}
+                            name={info?.name}
+                            fallback={info?.name?.[0] || "T"}
+                            borderColor={connectionIdToColor(connectionId)}
+                            />
+                            )
+                })}
+
+                {currentUser && (
+                    <UserAvatar 
+                    src = {currentUser.info?.picture}
+                    name={`${currentUser.info?.name} (You)`}
+                    fallback = {currentUser.info?.name?.[0]}
+                    borderColor={connectionIdToColor(currentUser.connectionId)}
+                    />
+                )}
+
+                {hasMoreUsers && (
+                    <UserAvatar
+                    name={`{user.length - MAX_SHOWN_USERS} more`}
+                    fallback = {`+${users.length - MAX_SHOWN_OTHER_USERS}`}
+                    />
+                )}
+            </div>
         </div>
     )
 }
