1bded0e69cb30046ac833f42b8eeb1a82fd7a2da
dhwanil005 <dhwanil.shah00@gmail.com>
2024-02-06 18:31:28 -0500

    Different types added to create less ambiguity about what happens when user selects one of the options in our side toolbar, next is displaying names attached to cursors so users know which cursor is whom(who is making the changes to the board)

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -3,8 +3,9 @@
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
-import { useSelf } from "@/liveblocks.config";
-
+import { useHistory, useCanRedo, useCanUndo, useSelf } from "@/liveblocks.config";
+import { useState } from "react";
+import { CanvasMode, CanvasState } from "@/types/canvas";
 
 interface CanvasProps {
     boardId: string;
@@ -12,12 +13,26 @@ interface CanvasProps {
 export const Canvas = ({
     boardId,
 }: CanvasProps) => {
+    const [canvasState,setCanvasState] = useState<CanvasState>({
+        mode:CanvasMode.None,
+    });
+
+    const history = useHistory();
+    const canUndo = useCanUndo();
+    const canRedo = useCanRedo();
     const info = useSelf((me)=>me.info);
     return (
         <main className="w-full h-full bg-neutral-100 touch-none relative">
             <Info boardId={boardId}/>
             <Participants/>
-            <Toolbar/>
+            <Toolbar
+                canvasState={canvasState}
+                setCanvasState={setCanvasState}
+                canRedo={canRedo}
+                canUndo={canUndo}
+                undo={history.undo}
+                redo={history.redo}
+            />
         </main>
     )
 }
