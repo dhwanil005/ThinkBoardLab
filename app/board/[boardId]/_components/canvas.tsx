64588ccd796280c37b0cf8ac17723ddb0f2b02b0
dhwanil005 <dhwanil.shah00@gmail.com>
2024-02-07 21:29:01 -0500

    User's cursors are now dispayed real time with their name attached. next is rendering the pre-defined shapes.

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -3,9 +3,16 @@
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
-import { useHistory, useCanRedo, useCanUndo, useSelf } from "@/liveblocks.config";
-import { useState } from "react";
-import { CanvasMode, CanvasState } from "@/types/canvas";
+import { useHistory,
+     useCanRedo,
+      useCanUndo, 
+      useSelf,
+      useMutation 
+    } from "@/liveblocks.config";
+import { useCallback, useState } from "react";
+import { Camera, CanvasMode, CanvasState } from "@/types/canvas";
+import { CursorsPresence } from "./cursors-presence";
+import { pointerEventToCanvasPoint } from "@/lib/utils";
 
 interface CanvasProps {
     boardId: string;
@@ -16,10 +23,35 @@ export const Canvas = ({
     const [canvasState,setCanvasState] = useState<CanvasState>({
         mode:CanvasMode.None,
     });
+    
+    const [ camera, setCamera] = useState<Camera>({x:0,y:0});
+
+    const onWheel = useCallback((e: React.WheelEvent)=>{
+        setCamera((camera)=>({
+            x: camera.x - e.deltaX,
+            y: camera.y - e.deltaY,
+        }));
+    },[]);
 
     const history = useHistory();
     const canUndo = useCanUndo();
     const canRedo = useCanRedo();
+
+    const onPointerMove = useMutation(({ setMyPresence }, e: React.PointerEvent)=> {
+        e.preventDefault();
+
+        const current = pointerEventToCanvasPoint(e, camera);
+        
+        setMyPresence({ cursor: current});
+    },[]);
+
+    const onPointerLeave = useMutation(({
+        setMyPresence
+    }) => {
+        setMyPresence({cursor: null});
+    },[]);
+
+
     const info = useSelf((me)=>me.info);
     return (
         <main className="w-full h-full bg-neutral-100 touch-none relative">
@@ -33,6 +65,15 @@ export const Canvas = ({
                 undo={history.undo}
                 redo={history.redo}
             />
+            <svg className="h-[100vh] w-[100vw]"
+                onWheel={onWheel}
+                onPointerMove={onPointerMove}
+                onPointerLeave={onPointerLeave}
+            >
+                <g>
+                    <CursorsPresence />
+                </g>
+            </svg>
         </main>
     )
 }
