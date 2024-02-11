75098a9aa71d02fef76ae85498d17a9a29850eef
dhwanil005 <dhwanil.shah00@gmail.com>
2024-02-11 02:45:45 -0500

    Change color for individual layer aka elements on board implemented, delete functionality also implemented, next is cascading layers above one another and getting a selection net thingy so that users can select multiple items

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -19,6 +19,7 @@ import { connectionIdToColor, pointerEventToCanvasPoint, resizeBounds } from "@/
 import { LiveObject } from "@liveblocks/client";
 import { LayerPreview } from "./layer-preview";
 import { SelectionBox } from "./selection-box";
+import { SelectionTools } from "./selection-tools";
 
 const MAX_LAYERS = 100;
 interface CanvasProps {
@@ -242,6 +243,10 @@ export const Canvas = ({
                 undo={history.undo}
                 redo={history.redo}
             />
+            <SelectionTools 
+                camera={camera}
+                setLastUsedColor={setLastUsedColor}
+                />
             <svg className="h-[100vh] w-[100vw]"
                 onWheel={onWheel}
                 onPointerMove={onPointerMove}
